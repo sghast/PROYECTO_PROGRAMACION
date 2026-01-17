374f838fa3d5eee36a163da70f3de6cf9f12edf5
Joshua Llumiquinga <62040407+Fantasmon0@users.noreply.github.com>
2026-01-16 20:56:55 -0500

    avanceUPDATEv2
    Menú interactivo de actualización y correcciones.

--- proyecto.cpp
+++ proyecto.cpp
@@ -10,16 +10,27 @@ struct Repuesto {
     int stock;
 };
 
-// ---- VALIDAR SI ID YA EXISTE ----
+//Menu de actualizacion de Producto
+void menuActualizacion(){
+    cout<<"OPCIONES PARA ACTUALIZAR PRODUCTO\n";
+    cout<<"1. Actualizar Nombre\n";
+    cout<<"2. Actualizar Marca\n";
+    cout<<"3. Actualizar Precio\n";
+    cout<<"4. Actualizar Stock\n";
+    cout<<"5. Salir\n";
+    cout<<"Seleccione opcion para actualizar producto: ";
+}
+
+
+// Verifica si un ID ya existe
 bool idExiste(int idBuscar) {
     ifstream archivo("repuestos.txt");
     string linea;
     int idArchivo;
 
-    if (!archivo) return false; // Si no existe el archivo, ningún ID existe aún
+    if (!archivo) return false;
 
     while (getline(archivo, linea)) {
-        // Extraer solo el ID hasta el primer '|'
         idArchivo = stoi(linea.substr(0, linea.find('|')));
         if (idArchivo == idBuscar) {
             archivo.close();
@@ -30,7 +41,7 @@ bool idExiste(int idBuscar) {
     return false;
 }
 
-//CREATE
+// CREATE
 void agregarRepuesto() {
     Repuesto nuevo;
     ofstream archivo("repuestos.txt", ios::app);
@@ -43,7 +54,6 @@ void agregarRepuesto() {
     cout << "ID: ";
     cin >> nuevo.id;
 
-    // ---- VALIDACIÓN DE ID ----
     if (idExiste(nuevo.id)) {
         cout << "Error: El ID ya existe. No se guardo el repuesto.\n";
         archivo.close();
@@ -58,7 +68,7 @@ void agregarRepuesto() {
     cout << "Marca: ";
     getline(cin, nuevo.marca);
 
-    cout << "Precio: ";
+    cout << "Precio: $";
     cin >> nuevo.precio;
 
     cout << "Stock: ";
@@ -74,6 +84,7 @@ void agregarRepuesto() {
     cout << "Repuesto guardado correctamente\n";
 }
 
+// READ
 void listarRepuestos() {
     ifstream archivo("repuestos.txt");
     string linea;
@@ -91,23 +102,189 @@ void listarRepuestos() {
     archivo.close();
 }
 
+// UPDATE
+void actualizarRepuesto() {
+    int idBuscado;
+    bool encontrado = false;
+    Repuesto producto;
+    string temp;
+
+    ifstream archivoOriginal("repuestos.txt");
+    ofstream archivoTemporal("temporal.txt");
+
+    if (!archivoOriginal) {
+        cout << "Error: No se encontro el archivo de repuestos.\n";
+        return;
+    }
+
+    cout << "Ingrese el ID del producto a actualizar: ";
+    cin >> idBuscado;
+
+    while (getline(archivoOriginal, temp, '|')) {
+        producto.id = stoi(temp);
+
+        getline(archivoOriginal, producto.nombre, '|');
+        getline(archivoOriginal, producto.marca, '|');
+
+        getline(archivoOriginal, temp, '|');
+        producto.precio = stof(temp);
+
+        getline(archivoOriginal, temp);
+        producto.stock = stoi(temp);
+
+        if (producto.id == idBuscado) {
+            encontrado = true;
+            cout << "\n--- Producto Encontrado ---\n";
+            cout << "Nombre actual: " << producto.nombre << endl;
+            cout << "Marca actual: "<< producto.marca <<endl;
+            cout << "Precio Actual: $"<< producto.precio << endl;
+            cout << "Stock Actual: "<< producto.stock <<endl;
+            
+            int opcionActualizar;
+            //MENU INTERACTIVO PARA ACTUALIZAR
+            do
+            {
+                menuActualizacion();
+                cin>>opcionActualizar;
+                cin.ignore();
+                if (opcionActualizar == 1)
+                {
+                    string nuevoNombre;
+                    cout<<"Ingrese nuevo nombre: ";
+                    getline(cin, nuevoNombre);
+                    cout<<"Se a actualizado el nombre "<<producto.nombre<<" a "<<nuevoNombre<<".\nVolviendo al menu.\n";
+                    producto.nombre = nuevoNombre;
+                } else if (opcionActualizar == 2)
+                {
+                    string nuevaMarca;
+                    cout<<"Ingrese nueva marca: ";
+                    getline(cin, nuevaMarca);
+                    cout<<"Se a actualizado la marca "<<producto.marca<<" a "<<nuevaMarca<<".\nVolviendo al menu.\n";
+                    producto.marca = nuevaMarca;
+                } else if (opcionActualizar == 3)
+                {
+                    float nuevoPrecio;
+                    cout<<"Ingrese el nuevo Precio: ";
+                    cin>>nuevoPrecio;
+                    cout<<"Se a actualizado el precio "<<producto.precio<<" a "<<nuevoPrecio<<".\nVolviendo al menu.\n";
+                    producto.precio = nuevoPrecio;
+                    cin.ignore();
+                } else if (opcionActualizar == 4)
+                {
+                    int nuevoStock;
+                    cout<<"Ingrese el nuevo Stock: ";
+                    cin>>nuevoStock;
+                    cout<<"Se a actualizado el stock "<<producto.stock<<" a "<<nuevoStock<<".\nVolviendo al menu.\n";
+                    producto.stock = nuevoStock;
+                    cin.ignore();
+                } else if (opcionActualizar == 5)
+                {
+                    cout<<"SALIENDO DEL MENU DE ACTUALIZACION DE PRODUCTOS. VOLVIENDO AL MENU PRINCIPAL.\n";
+                } else {
+                    cout<<"Opcion Invalida. Ingresar la opcion requerida\n";
+                }
+            } while (opcionActualizar != 5);
+            
+            
+            cin.ignore();
+        }
+
+        archivoTemporal << producto.id << "|"
+                        << producto.nombre << "|"
+                        << producto.marca << "|"
+                        << producto.precio << "|"
+                        << producto.stock << endl;
+    }
+
+    archivoOriginal.close();
+    archivoTemporal.close();
+
+    if (encontrado) {
+        remove("repuestos.txt");
+        rename("temporal.txt", "repuestos.txt");
+        cout << "\n¡Producto actualizado con éxito!\n";
+    } else {
+        remove("temporal.txt");
+        cout << "\nError: El ID no existe.\n";
+    }
+}
+
+// DELETE -> nueva función
+void eliminarRepuesto() {
+    int idEliminar;
+    bool eliminado = false;
+    Repuesto producto;
+    string temp;
+
+    ifstream archivoOriginal("repuestos.txt");
+    ofstream archivoTemporal("temporal.txt");
+
+    if (!archivoOriginal) {
+        cout << "Error: No se encontro el archivo de repuestos.\n";
+        return;
+    }
+
+    cout << "Ingrese el ID del repuesto a eliminar: ";
+    cin >> idEliminar;
+
+    while (getline(archivoOriginal, temp, '|')) {
+        producto.id = stoi(temp);
+
+        getline(archivoOriginal, producto.nombre, '|');
+        getline(archivoOriginal, producto.marca, '|');
+
+        getline(archivoOriginal, temp, '|');
+        producto.precio = stof(temp);
+
+        getline(archivoOriginal, temp);
+        producto.stock = stoi(temp);
+
+        if (producto.id == idEliminar) {
+            eliminado = true;
+        } else {
+            archivoTemporal << producto.id << "|"
+                            << producto.nombre << "|"
+                            << producto.marca << "|"
+                            << producto.precio << "|"
+                            << producto.stock << endl;
+        }
+    }
+
+    archivoOriginal.close();
+    archivoTemporal.close();
+
+    if (eliminado) {
+        remove("repuestos.txt");
+        rename("temporal.txt", "repuestos.txt");
+        cout << "\n¡Repuesto eliminado correctamente!\n";
+    } else {
+        remove("temporal.txt");
+        cout << "\nError: El ID no existe.\n";
+    }
+}
+
 int main() {
+    system("chcp 65001 > nul");
     int opcion;
 
     do {
         cout << "\n1. Agregar repuesto";
         cout << "\n2. Listar repuestos";
-        cout << "\n3. Salir";
+        cout << "\n3. Actualizar repuesto";
+        cout << "\n4. Eliminar repuesto";
+        cout << "\n5. Salir";
         cout << "\nOpcion: ";
         cin >> opcion;
 
         switch (opcion) {
             case 1: agregarRepuesto(); break;
             case 2: listarRepuestos(); break;
-            case 3: cout << "Saliendo...\n"; break;
+            case 3: actualizarRepuesto(); break;
+            case 4: eliminarRepuesto(); break;
+            case 5: cout << "Saliendo...\n"; break;
             default: cout << "Opcion invalida\n";
         }
-    } while (opcion != 3);
+    } while (opcion != 5);
 
     return 0;
 }
